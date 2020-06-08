9614e30404c3af121c2ddba59aeec24536a2867e
Mahsum Kocabey <mahsumkocabey@mac-mini.lan>
2020-06-08 13:15:47 +0100

    It is time to create mongoose in our application

---
+++ controllers/bootcamps.js
@@ -0,0 +1,40 @@
+// @dec     Get  all bootcamps
+// @route   Get /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+};
+
+// @dec     Get  single bootcamp
+// @route   Get /api/v1/bootcamps:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+};
+
+// @dec     Create new bootcamp
+// @route   Post /api/v1/bootcamps:id
+// @access  Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamps" });
+};
+
+// @dec     Update new bootcamp
+// @route   Put /api/v1/bootcamps:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// @dec     Delete bootcamp
+// @route   Post /api/v1/bootcamps:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+};
